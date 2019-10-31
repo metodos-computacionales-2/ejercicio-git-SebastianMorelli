a8db057fbceca756574e27d6fa397eeffe71b2a1
Default user <sm.morelli@uniandes.edu.co>
2019-10-31 21:15:36 +0000

    2.cpp

--- 2.cpp
+++ 2.cpp
@@ -4,4 +4,29 @@
 #include <cmath>
 using namespace std;
 
-//Declaración variables
+//Declaración variables.
+
+int a;
+int b;
+int c;
+
+int main()
+{
+    cout<<"Ingrese el primer cateto: ";
+    cin >> a;
+    
+    cout<<"Ingrese el segundo cateto: ";
+    cin >> b;
+    
+    cout<<"Ingrese la hipotenusa: ";
+    cin >> c;
+    
+    if(a*a + b*b == c*c){
+        cout<<"Los números pueden representar los lados de un triángulo rectángulo."<<endl;
+    }
+    
+    else{
+        cout<<"Los números no pueden representar los lados de un triángulo rectángulo."<<endl;
+    }
+    return 0;
+}
