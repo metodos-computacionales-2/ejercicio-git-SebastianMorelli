434a3c29aa859e3e6663925e98a6143ff9338ada
Default user <sm.morelli@uniandes.edu.co>
2019-10-31 21:06:02 +0000

    2.cpp

---
+++ 2.cpp
@@ -0,0 +1,7 @@
+// Este código verifica si 3 números pueden ser las medidas de los catetos de un triángulo rectángulo.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Declaración variables
