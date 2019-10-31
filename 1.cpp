baa0f39c0109b792e5c7c52fd407a11114eff735
Default user <sm.morelli@uniandes.edu.co>
2019-10-31 20:51:58 +0000

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -7,12 +7,13 @@ using namespace std;
 //Declaración de variables
 
 float r;
+float pi = 3.1416;
 
 int main()
 {
     cout<<"Ingrese el radio de un círculo: ";
     cin >> r;
-    
+    cout<<"El perímetro del círculo: "<< 2*pi*r << endl;
     
     return 0
 }
